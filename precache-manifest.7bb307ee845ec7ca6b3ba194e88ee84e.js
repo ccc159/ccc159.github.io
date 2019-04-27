835fa308d695bc1164a377e2405259a4d94d0de3
Jingcheng Chen <mail@chenjingcheng.com>
2019-04-27 21:17:54 +0200

    init commit

---
+++ precache-manifest.7bb307ee845ec7ca6b3ba194e88ee84e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67911e5516a43c9337876333c293a699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1737487492fa9092cd9",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "b9ae64c2b3f571ddb1f2",
+    "url": "/static/css/main.cc052908.chunk.css"
+  },
+  {
+    "revision": "e1737487492fa9092cd9",
+    "url": "/static/js/2.e9c90f9c.chunk.js"
+  },
+  {
+    "revision": "b9ae64c2b3f571ddb1f2",
+    "url": "/static/js/main.ec872f9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
